eabf1b5ad8794cc8b1b5a04e38b55f3ca0892109
kareenb <kareenb89@gmail.com>
2018-07-25 12:34:00 +0300

    issue routes full crudl; made changes to server

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 'use strict'
-const express = require('express')
-var cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-var bodyParser = require('body-parser')
+const issueRoutes = require('./routes/issueRoutes');
+const userRoutes = require('./routes/userRoutes');
+const commentRoutes = require('./routes/commentRoutes');
 
-
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors({
+    origin: ['http://localhost:8080'],
+    credentials: true
+}));
 app.use(express.static('dist'))
-
+app.use(cookieParser());
+app.use(session({
+    secret: 'fixity secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-
+issueRoutes(app);
+userRoutes(app);
+commentRoutes(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
